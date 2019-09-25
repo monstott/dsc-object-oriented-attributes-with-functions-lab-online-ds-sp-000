d0aec3a5e80d67860ecc6e4d633adc2fdda6af69
monstott <matthew.j.onstott@gmail.com>
2019-09-25 13:56:29 -0400

    finished lab

--- school.py
+++ school.py
@@ -1,1 +1,20 @@
 class School:
+    
+    def __init__(self, name):
+        self.name = name
+        self.roster = {}
+        
+    def add_student(self, fullname, grade):
+        if grade in self.roster:
+            self.roster[grade].append(fullname)
+        else:
+            self.roster[grade] = [fullname]
+
+    def grade(self, grade):
+           return self.roster[grade]
+        
+    def sort_roster(self):
+            sorted_roster = self.roster
+            for key in sorted_roster:
+                sorted_roster[key].sort()
+            return sorted_roster
